5f411ca604d1eb215159330b334461cdf519a153
Rustam Zagirov <rustam@zagirov.name>
2011-01-15 05:41:10 +0300

    Adding localization handlers.js to russian

--- CSwfUpload.php
+++ CSwfUpload.php
@@ -12,19 +12,31 @@ class CSwfUpload extends CWidget
 
 		$cs->registerScript(__CLASS__.'swfuv',"var swfuPath='" . $baseUrl . "';", CClientScript::POS_HEAD);
 		$cs->registerScriptFile($baseUrl . '/swfupload.js', CClientScript::POS_HEAD);
-		$cs->registerScriptFile($baseUrl . '/handlers.js', CClientScript::POS_END);
 		$cs->registerCssFile($baseUrl . '/swfupload.css');
+
+		//check existing localization file handlers
+		if (file_exists($assets . '/handlers_' . Yii::app()->language . '.js'))
+		{
+			$handlers = $baseUrl . '/handlers_' . Yii::app()->language . '.js';
+		}
+		else
+		{
+			$handlers = $baseUrl . '/handlers.js';
+		}
+		$cs->registerScriptFile($handlers, CClientScript::POS_END);
+
 
 		$postParams = array('PHPSESSID'=>session_id());
 		if(isset($this->postParams))
 		{
-				$postParams = array_merge($postParams, $this->postParams);
+			$postParams = array_merge($postParams, $this->postParams);
 		}
 		$config = array(
 			'post_params'=> $postParams,
 			'flash_url'=> $baseUrl. '/swfupload.swf',
 			'button_image_url'=> $baseUrl .'/images/SmallSpyGlassWithTransperancy_17x18.png',
-		);	//'jsHandlerUrl'=>$baseUrl.'/handlers.js' //Relative path
+			'jsHandlerUrl'=>$handlers,
+		);
 
 		$config = array_merge($config, $this->config);
 		$config = CJavaScript::encode($config);
